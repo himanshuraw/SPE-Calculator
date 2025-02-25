d469ff2d2cb1dca92e3097177f5737e16999ea3e
himanshuraw <himanshurawat180@gmail.com>
2025-02-25 16:36:31 +0530

    Created Basic ScientificCalculator class

---
+++ src/main/java/com/himanshu/Main.java
@@ -0,0 +1,7 @@
+package com.himanshu;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ src/main/java/com/himanshu/ScientificCalculator.java
@@ -0,0 +1,23 @@
+package com.himanshu;
+
+public class ScientificCalculator {
+    public double sqrt(double n){
+        return Math.sqrt(n);
+    }
+
+    public long factorial(int n){
+        long ans = 1;
+        for(int i = 2; i<=n; i++){
+            ans *= i;
+        }
+        return ans;
+    }
+
+    public double naturalLog(double n){
+        return Math.log(n);
+    }
+
+    public double power(double n, double exp){
+        return Math.pow(n, exp);
+    }
+}
