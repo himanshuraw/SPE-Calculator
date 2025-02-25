f74247fd095eecaecb3857b8f1e2b8a1e56c4f84
himanshuraw <himanshurawat180@gmail.com>
2025-02-25 16:39:46 +0530

    Created Basic ScientificCalculator class

--- src/main/java/com/himanshu/ScientificCalculator.java
+++ src/main/java/com/himanshu/ScientificCalculator.java
@@ -1,6 +1,26 @@
 package com.himanshu;
 
 public class ScientificCalculator {
+    public long add(long a, long b) {
+        return a + b;
+    }
+
+    public long substract(long a, long b) {
+        return a - b;
+    }
+
+    public long multiply(long a, long b) {
+        return a * b;
+    }
+
+    public long divide(long a, long b) {
+        return a / b;
+    }
+
+    public long mod(long a, long b) {
+        return a % b;
+    }
+
     public double sqrt(double n){
         return Math.sqrt(n);
     }
